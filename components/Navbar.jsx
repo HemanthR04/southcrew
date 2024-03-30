b60b040ead7355f6d5313974e3cd49f787d18753
hr3003 <hr3003@att.com>
2024-03-30 16:49:36 +0530

    Southcrew

---
+++ components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+
+      <ul className='w-full h-20 flex justify-between items-center px-10 text-white'>
+        <li>What we do?</li>
+        <li>SOUTHCREW</li>
+        <li>Call us</li>
+      </ul>
+
+    </nav>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      backgroundColor:{
+        "primary":"#1A1A1A",
+        "secondary":"#161616"
+      },
+      textColor:{
+        "primary":"#CCCCCC"
+      }
     },
   },
   plugins: [],
